a98cc6731decb8eb03aefc33bd4fb5104d62d33f
don <vinvintage111@gmail.com>
2026-04-09 03:27:40 +0800

    improved jobID retrieval and tested the teleport via jobID (working)

--- checkServer.lua
+++ checkServer.lua
@@ -1,8 +1,9 @@
 getgenv().config = {
     ping_when_coin = 0 -- ping when coin of the brainrot is greater than or equal to this value
     webhook = "WEBHOOK_URL" -- webhook url to send the notification to
-    min_player_count = 1 -- minimum player count in the server for the bot to join
-    fullhop_count = 1 -- number of times the bot wiil attenpt to enter the full server
+    min_player_count = 1, -- minimum player count in the server for the bot to join
+    fullhop_count = 1, -- number of times the bot wiil attenpt to enter the full server
+    bot_stay = 2 -- time in minutes the bot will stay in the server before hopping to another one (if the coin is not found)
 }
 
 repeat wait() until game:IsLoaded() -- wait until the game is loaded
@@ -22,21 +23,67 @@ local oldServerID = {}  -- stores the server IDs that have been checked
 --roblox functionalities
 local teleportService = game:GetService("TeleportService")
 local httpService = game:GetService("HttpService")
-local API = "https://games.roblox.com/v1/games/" .. sabPlaceID .. "/servers/Public?limit=100" -- API to get the list of public servers for the sab place
+local API = "https://steal-a-brainrot-server-retrieval.onrender.com/test" -- API to get the list of public servers for the sab place
 
---get jobID
-local function getJobID() 
-    local success, result = pcall(function() return httpService:GetAsync(API) end)
-    task.wait(1) -- wait for 1 second to avoid spamming the API
+-- get the right http function for any executor
+local function getHttp()
+    return http_request or request or http and http.request
+end
+
+local function getJobID()
+    local http = getHttp() --stores the http request found that the executor supports
+    
+    --check if the executor supports any http request
+    if not http then
+        print("No http function found!")
+        return nil
+    end
+    
+    -- retrieve raw server list from the api of roblox
+    local success, result = pcall(function()
+        return http({
+            Url = API,
+            Method = "GET",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            }
+        })
+    end)
+    
+    task.wait(1)
+    
     if success then
-        local data = httpService:JSONDecode(result) -- decode the JSON response from the API
+        -- result.Body contains the response
+        local data = httpService:JSONDecode(result.Body)
+        
+        --checks if the data table exist in the api
+        if not data or not data.data then
+            print("No server data found! / game is down")
+            return nil
+        end
+        
+        local jobIDStorage = {} --stores jobIDs
+
+        --main jobid retrieving process
         for _, server in pairs(data.data) do
-            if server.playing >= getgenv().config.min_player_count then --checks if the server has the minimum player count
-                return server.id -- return the server ID if it has the minimum player count
+            if server.playing >= getgenv().config.min_player_count -- greater or equal to the min player count
+            and server.playing < server.maxPlayers  -- not full
+            and server.id ~= game.JobId then        -- not current server
+                table.insert(jobIDStorage, server.id)
             end
         end
+
+        --return valid random job ID
+        if #jobIDStorage > 0 then
+            local randomJobID = jobIDStorage[math.random(1, #jobIDStorage)] -- get a random job ID from the storage
+            return randomJobID
+        end
+        
+        print("No servers found with minimum player count!")
+        return nil
     else
         print("Error getting server list: " .. result)
+        return nil
     end
 end
 
@@ -56,23 +103,25 @@ end
 ]]
 
 --check if the player got kicked then teleport to the same server
+--status: to be fixed (near)
 local function checkKicked()
     game:GetService('CoreGui').RobloxPromptGui.promptOverlay.ChildAdded
     :Connect(function(child)
         if child.Name == "ErrorPrompt" then
             local jobID = getJobID()
             task.wait(0.5)
+            local hopcount = 0
             repeat
-                local hopcount = 0
                 pcall(function() teleportService:TeleportToPlaceInstance(sabPlaceID, jobID, player) end)
                 hopcount = hopcount + 1
                 task.wait(5)
-            until character or getgenv().config.fullhop_count == hopcount
-            end
+            until character or hopcount >= getgenv().config.fullhop_count
         end
     end)
+end
 
 --check for brainrots in the server
+--not pa finish
 local function checkBrainrots()
     --debug purposes
     local success, brainrot = pcall(function() return workspace:FindFirstChild("Brainrot") end)
@@ -80,16 +129,21 @@ local function checkBrainrots()
         for _, value in pairs(brainrot) do 
 
 -- main process
+checkKicked() -- check if the player got kicked and teleport back to the same server if true
+
 while true do 
     local jobID = getJobID() --stores job ID
+    print(jobID)
     if jobID then 
-        if not table.find(oldServerID, jobID) then -- checks if the jobID exist in oldServerID
-            table.insert(oldServerID, jobID)
-            teleportService:TeleportToPlaceInstance(sabPlaceID, jobID, player)
-            task.wait(5)
-            
+        task.wait(getgenv().config.bot_stay) --check if the player got kicked and teleport back to the same server if true
+        local success, result = pcall(function() teleportService:TeleportToPlaceInstance(sabPlaceID, jobID, player) end) --teleport to the server with the job ID
+        if not success then
+            print("Teleport failed:", result)  -- see error
         end
+    else 
+        task.wait(5)
     end
+end
 
     
 
