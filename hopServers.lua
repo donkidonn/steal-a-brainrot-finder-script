a0933dcb9baf863cb087715d05a69cbf5c7410df
don <vinvintage111@gmail.com>
2026-04-14 03:26:19 +0800

    forgot to change min count to 1 in hopserver

--- hopServers.lua
+++ hopServers.lua
@@ -223,7 +223,7 @@ local function getJobID()
 
         --main jobid retrieving process
         for _, server in pairs(data.data) do
-            if server.playing >= getgenv().config.min_player_count -- greater or equal to the min player count
+            if server.playing >= 1 -- greater or equal to the min player count
             and server.playing < server.maxPlayers  -- not full
             and server.id ~= game.JobId then        -- not current server
                 table.insert(jobIDStorage, server.id)
